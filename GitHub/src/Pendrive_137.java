0ba2f464880491bf8fdc0422dece367bcad747b4
muska <muska@LAPTOP-DB02CP52>
2022-06-14 06:21:33 +1400

    Commit my first change

---
+++ GitHub/src/Pendrive_137.java
@@ -0,0 +1,14 @@
+
+public class Pendrive_137 {
+	int size;
+	Pendrive_137(int size)
+	{
+		this.size=size;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
